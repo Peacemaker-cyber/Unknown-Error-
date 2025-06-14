45eb917947094f42bc0d7b5964c7fdc1b5b87918
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-06-14 09:20:09 +0300

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -6,7 +6,7 @@ const repo = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(" ")[0].toLowerCase() : "";
 
   if (["repo", "sc", "script", "info"].includes(cmd)) {
-    const githubRepoURL = "https://github.com/Black-Tappy/XEON-XMD";
+    const githubRepoURL = "https://github.com/Peacemaker-cyber/PEACE-MD";
 
     try {
       // Extract username and repo name from the URL
